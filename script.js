57e4d8592f6a45e4d98d165ea50dc475cc9f4f37
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-08 15:01:47 +0100

    aggiunta console.log

--- script.js
+++ script.js
@@ -9,4 +9,25 @@ numero2 = parseInt(numero2);
 
 numeroDiviso = numero1 / numero2
 
+password = nome + cognome + colorePreferito + numeroDiviso
 
+console.log(
+    `
+    ------ Informazioni Anagrafiche ------
+    Nome: ${nome}
+    Cognome ${cognome}
+
+    ------ Colore Preferito ------
+    Colore Preferito: ${colorePreferito}
+
+    ------ Numeri Inseriti ------
+    Numero 1: ${numero1}
+    Numero 2: ${numero2}
+
+    ------ Risultato Divisione Numeri ------
+    Risultato: ${numeroDiviso}
+
+    ------ Password Generata ------
+    Password: ${password}
+    `
+)
