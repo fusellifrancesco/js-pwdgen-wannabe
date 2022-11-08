9e50057e953aa4d802e9562ad10b91835f1884a6
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-08 14:43:12 +0100

    correzzione titolo html e aggiunte variabili in JS

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome = prompt("Inserisci il tuo nome");
+let cognome = prompt("Inserisci il tuo cognome");
+let colorePreferito = prompt("Inserisci il tuo colore preferito");
+let numero1 = prompt("Inserisci un numero");
+let numero2 = prompt("Inserisci un altro numero");
+
+
