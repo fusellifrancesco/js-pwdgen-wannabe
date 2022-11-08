b0385ff46a4299bf8babdb2d470f6d7bb4bef884
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-11-08 14:51:00 +0100

    aggiunte variabili numeriche ed op. aritmetica

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ let colorePreferito = prompt("Inserisci il tuo colore preferito");
 let numero1 = prompt("Inserisci un numero");
 let numero2 = prompt("Inserisci un altro numero");
 
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+
+numeroDiviso = numero1 / numero2
+
 
